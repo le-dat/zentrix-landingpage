30308ad2590f73c8297b7e54f1340a99527675c2
le-dat <lqdat87@gmail.com>
2026-04-13 20:04:06 +0700

    fix: fix build and implement vercel/seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwind()],
   },
